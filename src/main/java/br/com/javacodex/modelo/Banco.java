068bd84924b391f7ef5898b8dd98d4f320bacf06
Alex Leite <71527190+AlexLeite-Hub@users.noreply.github.com>
2021-09-05 19:29:22 -0300

    Criando autenticação e autorização para o CRUD

--- src/main/java/br/com/javacodex/modelo/Banco.java
+++ src/main/java/br/com/javacodex/modelo/Banco.java
@@ -9,6 +9,7 @@ import br.com.javacodex.modelo.Cliente;
 public class Banco {
 	
 	private static List<Cliente> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	
@@ -29,6 +30,18 @@ public class Banco {
 
 		lista.add(cliente);
 		lista.add(cliente2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("Alex");
+		u1.setSenha("01234");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("Flávia");
+		u2.setSenha("56789");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
+		
 		}
 	
 
@@ -69,6 +82,15 @@ public class Banco {
 		for(Cliente cliente : lista) {
 			if(cliente.getId() == id) {
 				return cliente;
+			}
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for (Usuario usuario: listaUsuarios) {
+			if(usuario.heIgual(login, senha)) {
+				return usuario;
 			}
 		}
 		return null;
