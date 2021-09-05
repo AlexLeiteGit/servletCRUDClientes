6bc6fda8d66e517f5136aa4c3759ed8440142577
Alex Leite <71527190+AlexLeite-Hub@users.noreply.github.com>
2021-09-05 20:15:45 -0300

    Implementando sessão no projeto CRUD

--- src/main/java/br/com/javacodex/acao/ListaCliente.java
+++ src/main/java/br/com/javacodex/acao/ListaCliente.java
@@ -8,6 +8,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.javacodex.modelo.Banco;
 import br.com.javacodex.modelo.Cliente;
@@ -15,6 +16,12 @@ import br.com.javacodex.modelo.Cliente;
 public class ListaCliente implements Acao{
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession sessao = request.getSession();
+		if(sessao.getAttribute("usuarioLogado") == null) {
+			return "redirect:entrada?acao=LoginForm";
+			
+		}
 		
 		System.out.println("Listando Empresas");
 		
